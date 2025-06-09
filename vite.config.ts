b6c9b7fa78b080afaa26d6647cae6a4e4446c5cf
Enes Toraman <enes.toraman@layersup.dev>
2025-06-09 16:10:25 +0300

    Fix for the sandbox

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,9 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "src/components"),
     },
   },
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true
+}
 });
